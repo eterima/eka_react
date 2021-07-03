f041c6efdaf7949e90b877f9a4268cc13832a6e5
Eka <putu_eka_terima@yahoo.de>
2021-07-04 00:45:49 +0200

    -.
    dot

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Become the next epilot unicorn!.
+          Become the next epilot unicorn!
         </p>
         <a
           className="App-link"
