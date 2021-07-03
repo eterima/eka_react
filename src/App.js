06fe6225961d103f8c17876465a5b9045106c7dd
Eka <putu_eka_terima@yahoo.de>
2021-07-04 00:42:09 +0200

    link
    Link

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Become the next epilot unicorn!.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://promise.epilot.cloud"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Our promise
         </a>
       </header>
     </div>
