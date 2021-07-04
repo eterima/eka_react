2bd4480e96c37d3531bd128f146a9b6718c5478d
Eka <putu_eka_terima@yahoo.de>
2021-07-04 08:41:37 +0200

    joke

--- src/App.js
+++ src/App.js
@@ -6,16 +6,33 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Become the next epilot unicorn!
-        </p>
+        <p>  
+          Hello epilots!
+          </p>
+          
+          <p>
+          Welcome to THE BEST React app anyone ever built! ... NOT. 
+          Well this was of course just a joke :D 
+          </p>
+
+          <p>
+          Well ok. So that you are maybe less disappointed here a joke for you:
+          </p>
+
+          <p>
+          3 Database SQL walked into a NoSQL bar. A little while later. 
+          They walked out
+          </p>
+          <p>
+          Because they couldn’t find a table!
+          </p>
         <a
           className="App-link"
-          href="https://promise.epilot.cloud"
+          href="https://epilot.slack.com/archives/C01E7K2RXPA"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Our promise
+          Leave a comment in fun-and-rockets
         </a>
       </header>
     </div>
