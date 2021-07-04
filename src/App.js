a09daff3c80e750accfe3687a7cb849001e6b3d3
Eka <putu_eka_terima@yahoo.de>
2021-07-04 09:00:59 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,10 +14,7 @@ function App() {
           Welcome to THE BEST React app anyone ever built! ... NOT. 
           Well this was of course just a joke :D 
           </p>
-          <p>
-          But it was actually really interesting to get a very, very small glimps of the life of a Dev :P :D 
-          </p>
-
+      
           <p>
           Next time I will release something awesome.
           </p>
