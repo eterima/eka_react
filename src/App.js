b6fc2d877b5188c793ca7f95f20e3aed4ba8fe37
Eka <putu_eka_terima@yahoo.de>
2021-07-06 08:21:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>  
-          Hello epilots!
+          Hello Momo!
           </p>
           
           <p>
