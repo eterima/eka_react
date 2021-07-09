19382297a0b9728789cbd72f1bb2f53191b296c6
Eka <putu_eka_terima@yahoo.de>
2021-07-09 23:19:37 +0200

    comment

--- src/App.js
+++ src/App.js
@@ -2,6 +2,71 @@ import logo from './logo.png';
 import gif from './Jobs GIF-downsized_large (1).gif';
 import './App.css';
 
+import React, { PureComponent, useState } from 'react'
+import data from "./data.json"
+import { CommentSection } from 'react-comments'
+import 'react-comments/dist/index.css'
+import "./App.css"
+
+
+ const App = () => {
+   const [comment, setComment] = useState(data)
+   const userId = "01a"
+   const avatarUrl = "https://ui-avatars.com/api/name=Riya&background=random"
+   const name = "xyz"
+   const signinUrl = "/signin"
+   const signupUrl = "/signup"
+   let count = 0
+   comment.map(i => {count+=1; i.replies && i.replies.map(i=> count+=1)} )
+
+   return <div className="commentSection">
+ <div className="header">{count} Comments</div>
+
+ <CommentSection currentUser={userId && { userId: userId, avatarUrl: avatarUrl, name: name }} commentsArray={comment}
+ setComment={setComment} signinUrl={signinUrl} signupUrl={signupUrl}/>
+     </div>
+ }
+ 
+ export default App
+
+ import React, { PureComponent, useState } from 'react'
+ import data from "./data.json"
+ import { CommentSection } from 'react-comments'
+ import 'react-comments/dist/index.css'
+ import "./App.css"
+ 
+ class App extends PureComponent{
+   state={ 
+     comment : data
+   }
+ 
+   handleState=(value)=>{
+     this.setState({comment:value})
+   }
+ 
+   render() {
+     const userId = "01a"
+     const avatarUrl = "https://ui-avatars.com/api/name=Riya&background=random"
+     const name = "xyz"
+     const signinUrl = "/signin"
+     const signupUrl = "/signup"
+     let count = 0
+    this.state.comment.map(i => {count+=1; i.replies && i.replies.map(i=> count+=1)} )
+ 
+     return <div className="commentSection">
+      <div className="header">{count} Comments</div>
+     
+      <CommentSection currentUser={userId && { userId: userId, avatarUrl: avatarUrl, name:name }} commentsArray={this.state.comment}
+      setComment={this.handleState} signinUrl={signinUrl} signupUrl={signupUrl} />
+          </div>
+ }
+ }
+ 
+ export default App
+ 
+ 
+
+
 function App() {
   return (
     <div className="App">
@@ -60,8 +125,13 @@ function App() {
           </p>
 
         </a>
+
+        
       </header>
+      
     </div>
+    
+    
   );
 }
 
